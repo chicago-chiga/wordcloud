54eb5b1971795d9f610d7fdaa899e66842ab24d9
chiga <chicago.chiga@gmail.com>
2022-06-07 02:35:38 +0900

    Update: st_create_wordcloud.py
    refs #11 単語カウント結果をStreamlit版でも表示する
    refs #12 Downloadボタンを押すと、表示内容がクリアされる

--- src/st_create_wordcloud.py
+++ src/st_create_wordcloud.py
@@ -2,11 +2,9 @@ import csv
 import os
 import tempfile
 import time
-# from cProfile import label
 from pathlib import Path
-# from re import S
 
-# import pandas as pd
+import pandas as pd
 import streamlit as st
 from PIL import Image
 
@@ -19,22 +17,38 @@ def create_temp_dir():
     return dname
 
 
+def keep_count_result(count_result):
+    st.session_state['count_result'] = count_result
+
+
+def keep_wc_result(wc):
+    st.session_state['wc'] = wc
+
+
 def main():
     st.set_page_config(layout="wide")
     st.sidebar.header('テキスト分析ツール')
     st.sidebar.text('welcome!')
 
     # 一時ディレクトリ作成
-
     dname = create_temp_dir()
 
-    # サイドバー
+    # セッション初期化
+    if 'count_result' not in st.session_state:
+        st.session_state['count_result'] = None
+
+    if 'wc' not in st.session_state:
+        st.session_state['wc'] = None
+
+    count_result = pd.DataFrame(index=[], columns=[])
+    wc = None
+
+    # サイドバーエリア
     st.sidebar.markdown('### 各種設定')
 
     # ユーザー辞書
     st.sidebar.markdown('**ユーザー辞書**')
     user_dic = st.sidebar.file_uploader(label='単語登録が必要なときだけ:')
-    st.sidebar.write('user_dic: ', user_dic)
 
     if user_dic is not None:
         # XXX: 信頼できないファイルは安易に評価しないこと
@@ -46,14 +60,13 @@ def main():
     # font_file
     st.sidebar.markdown('**フォントファイル [必須]**')
     font_file = st.sidebar.file_uploader(label='TTF形式のみ:')
-    st.sidebar.write('font_file: ', font_file)
 
     if font_file is not None:
         with open(os.path.join(dname.name, font_file.name), "wb") as tmp_font_file:
             font_path = Path(tmp_font_file.name)
             font_path.write_bytes(font_file.getvalue())
 
-     # udic_type
+    # udic_type
     st.sidebar.markdown('**辞書タイプ**')
     udic_type = st.sidebar.selectbox(
         'simpledic: 簡易版, ipadic: IPA形式',
@@ -76,7 +89,6 @@ def main():
     # input file
     st.markdown('**分析対象ファイル [必須]**')
     input_txt_file = st.file_uploader(label='utf-8形式:')
-    st.write('input_txt_file: ', input_txt_file)
 
     if input_txt_file is not None:
         # XXX: 信頼できないファイルは安易に評価しないこと
@@ -88,35 +100,44 @@ def main():
     st.markdown('**WordCloudのダウンロードファイル名 [必須]**')
     wc_file = st.text_input(label='拡張子は「.png」を指定してください:')
 
+    # wordcloud
+    wc_file_path = os.path.join(dname.name, wc_file)
+
+    # 単語カウント結果ファイル
+    count_file_name = (os.path.splitext(
+        os.path.basename(wc_file_path))[0]+'.txt')
+
+    # ボタン活性化制御
     if font_file is None or input_txt_file is None or not wc_file:
         disable_flg = True
     else:
         disable_flg = False
 
+    # 文字数カウント結果表示
+    count_flg = st.checkbox('単語カウント結果も出力', disabled=disable_flg)
+
+    # 分析処理
     start = st.button("分析開始！", disabled=disable_flg)
 
-    # split テキスト
     if start:
+        # Sessionクリア
+        if 'count_result' in st.session_state.keys():
+            del st.session_state['count_result']
+        if 'wc' in st.session_state.keys():
+            del st.session_state['wc']
 
         if user_dic is None:
             tmp_user_dic_name = ""
         else:
             tmp_user_dic_name = tmp_user_dic.name
 
+        # 単語分割
         split_text = mw.split_text(
             tmp_input.name, tmp_user_dic_name, stop_words, udic_type)
 
         with open(os.path.join(dname.name, 'split_word.txt'), "w",  encoding='utf-8') as tmp_split_word:
             writer = csv.writer(tmp_split_word, delimiter=' ')
             writer.writerows(split_text)
-
-        # Todo:カウントファイルは後で考える
-        # count_word = mw.count_word(
-        #     split_file_path, top_rank, tmp_user_dic.name, udic_type, dname)
-        # st.write('count_word_list: ', count_word_list)
-
-        # wordcloud
-        wc_file_path = os.path.join(dname.name, wc_file)
 
         # progress bar
         text = st.empty()
@@ -127,20 +148,49 @@ def main():
             bar.progress(i + 1)
             time.sleep(0.01)
 
+        # WordCloud作成
         wc = mw.put_wordcloud(
             tmp_split_word.name, wc_file_path, tmp_font_file.name)
-        if wc:
+        keep_wc_result(wc)
 
-            wc_img = Image.open(wc_file_path)
-            st.image(wc_img)
+        # 単語カウント結果作成
+        if count_flg:
+            count_word = mw.count_word(tmp_split_word.name,
+                                       top_rank, tmp_user_dic_name, udic_type)
+            count_result = pd.DataFrame(
+                count_word, columns=["単語  ", "出現数      "])
+            keep_count_result(count_result)
 
-            with open(wc_file_path, "rb") as img_file:
-                btn = st.download_button(
-                    label="Download image",
-                    data=img_file,
-                    file_name=wc_file,
-                    mime="image/png"
-                )
+    # 結果表示エリア
+    if st.session_state.count_result is not None:
+        with st.expander("単語カウント結果"):
+            # sessionの結果を引き継ぐ
+            session_result = st.session_state.count_result
+            st.dataframe(session_result)
+
+            count_file = session_result.to_csv(
+                sep='\t', index=True).encode('utf8')
+
+            dl_count_btn = st.download_button(
+                label="Download",
+                data=count_file,
+                file_name=count_file_name,
+                on_click=keep_count_result(session_result),
+                mime='text/csv')
+
+    if st.session_state.wc is not None:
+        wc_img = Image.open(wc_file_path)
+        st.image(wc_img)
+
+        # wc_imgをセッションに入れる
+        with open(wc_file_path, "rb") as img_file:
+            dl_wc_btn = st.download_button(
+                label="Download image",
+                data=img_file,
+                file_name=wc_file,
+                on_click=keep_wc_result(wc_img),
+                mime="image/png"
+            )
 
 
 if __name__ == '__main__':
